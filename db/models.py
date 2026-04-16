f08927f36f6773d45abc9a9c176705af9b4746c8
nascimento140594 <nascimento140594@gmail.com>
2026-04-16 20:34:21 -0300

    Update string representation methods and fix formatting

--- db/models.py
+++ db/models.py
@@ -1,10 +1,12 @@
+from __future__ import annotations
+
 from django.db import models
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
+    def __str__(self: 'Genre') -> str:
         return self.name
 
 
@@ -12,17 +14,17 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+    def __str__(self: 'Actor') -> str:
+        return f'{self.first_name} {self.last_name}'
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(Actor, related_name="movies")
-    genres = models.ManyToManyField(Genre, related_name="movies")
+    actors = models.ManyToManyField(Actor, related_name='movies')
+    genres = models.ManyToManyField(Genre, related_name='movies')
 
-    def __str__(self) -> str:
+    def __str__(self: 'Movie') -> str:
         return self.title
 
 
@@ -32,10 +34,10 @@ class CinemaHall(models.Model):
     seats_in_row = models.IntegerField()
 
     @property
-    def capacity(self) -> int:
+    def capacity(self: 'CinemaHall') -> int:
         return self.rows * self.seats_in_row
 
-    def __str__(self) -> str:
+    def __str__(self: 'CinemaHall') -> str:
         return self.name
 
 
@@ -44,16 +46,16 @@ class MovieSession(models.Model):
     cinema_hall = models.ForeignKey(
         CinemaHall,
         on_delete=models.CASCADE,
-        related_name="movie_sessions",
+        related_name='movie_sessions',
     )
     movie = models.ForeignKey(
         Movie,
         on_delete=models.CASCADE,
-        related_name="movie_sessions",
+        related_name='movie_sessions',
     )
 
-    def __str__(self) -> str:
+    def __str__(self: 'MovieSession') -> str:
         return (
-            f"{self.movie.title} "
-            f"{self.show_time.strftime('%Y-%m-%d %H:%M:%S')}"
+            f'{self.movie.title} '
+            f'{self.show_time.strftime("%Y-%m-%d %H:%M:%S")}'
         )
