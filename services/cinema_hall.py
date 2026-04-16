9fa016da2c5cd24fa25e7afe221f48d67fa9323b
nascimento140594 <nascimento140594@gmail.com>
2026-04-16 20:34:51 -0300

    Update cinema_hall.py

--- services/cinema_hall.py
+++ services/cinema_hall.py
@@ -1,7 +1,9 @@
+from django.db.models import QuerySet
+
 from db.models import CinemaHall
 
 
-def get_cinema_halls():
+def get_cinema_halls() -> QuerySet[CinemaHall]:
     return CinemaHall.objects.all()
 
 
