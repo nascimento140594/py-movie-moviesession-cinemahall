f81dcedbc9c712ca415d0d2254545fa278cb2b3b
nascimento140594 <nascimento140594@gmail.com>
2026-04-16 20:35:33 -0300

    Add type hint for get_movies return value

--- services/movie.py
+++ services/movie.py
@@ -1,10 +1,12 @@
+from django.db.models import QuerySet
+
 from db.models import Movie
 
 
 def get_movies(
     genres_ids: list[int] | None = None,
     actors_ids: list[int] | None = None,
-):
+) -> QuerySet[Movie]:
     movies = Movie.objects.all()
 
     if genres_ids:
